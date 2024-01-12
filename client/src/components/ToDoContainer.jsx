f0b387da973ad31ed3c954693663fe870e12e407
Naspo <a.topaltsis@gmail.com>
2024-01-11 19:13:52 -0500

    Started work on ToDoContainer

--- client/src/components/ToDoContainer.jsx
+++ client/src/components/ToDoContainer.jsx
@@ -1,5 +1,44 @@
+// ToDoContainer Component
+
+const daysOfTheWeek = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
 function ToDoContainer() {
-  return <div className="ToDoContainer"></div>;
+  let currentDate = new Date();
+  let writtenDay = daysOfTheWeek[currentDate.getDay()];
+  let writtenMonth = months[currentDate.getMonth()];
+  let fullDateFormatted = `${writtenDay}, ${writtenMonth} ${currentDate.getDate()}, ${currentDate.getFullYear()}`;
+
+  return (
+    <div className="ToDoContainer">
+      <div className="heading-area">
+        <h1 className="main-heading">My To-Do List</h1>
+        <h2 className="date-subheading">{fullDateFormatted}</h2>
+      </div>
+    </div>
+  );
 }
 
 export default ToDoContainer;
