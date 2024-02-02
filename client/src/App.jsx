f25c53d7fb2d66d6e6554ca39d86348fbd0cbe1d
Naspo <a.topaltsis@gmail.com>
2024-02-02 15:18:33 -0500

    Started work on crediting myself

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,26 @@ function App() {
   return (
     <div className="App">
       <ToDoContainer />
+      <div className="footer">
+        <a
+          href="https://github.com/NaspoDev/To-Do-WebApp"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            className="github-icon"
+            src="./src/assets/images/github_icon.png"
+            alt="Github"
+          />
+        </a>
+        <p className="credit-tag">
+          Created by{" " /* Add a space before "Naspo"*/}
+          <a href="https://naspo.dev/" target="_blank" rel="noreferrer">
+            Naspo
+          </a>
+          .
+        </p>
+      </div>
     </div>
   );
 }
